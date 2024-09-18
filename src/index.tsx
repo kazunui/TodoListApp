5342e6d44b9afc4c480f1e5cb8467437712bd650
kazunui <enokazu7563@gmail.com>
2024-09-18 11:42:39 +0900

    react-helmetの追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>TodoListApp</title>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
